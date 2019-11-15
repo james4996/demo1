eafefc7f61aa271e91b314b60e43b7b50f2602cf
James <james@tantami.co.uk>
2019-11-15 20:59:04 +0000

    Started Styling

--- src/App.js
+++ src/App.js
@@ -1,24 +1,19 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <input type="checkbox"/>
+      <p>Test 1</p>
+
+      <input type="checkbox"/>
+      <p>Test 2</p>
+
+      <input type="checkbox"/>
+      <p>Test 3</p>
+
+      <input type="checkbox"/>
+      <p>Test 4</p>
     </div>
   );
 }
